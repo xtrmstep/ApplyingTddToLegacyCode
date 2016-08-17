32e6955b0c0c5b2d09fbb6b713a7c9c3aa14bbb7
Alexander Goida <alexander.goida@gmail.com>
2016-08-17 19:59:29 +0300

    Added example from rule 1

---
+++ ApplyingTddToLegacyCode/Rule2Example/Shop/EuropeShop.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Rule2Example.Taxes;
+
+namespace Rule2Example.Shop
+{
+    public class EuropeShop : Shop
+    {
+        public override void CreateSale()
+        {
+            var taxes = new EuropeTaxes();
+            var items = LoadSelectedItemsFromDb();
+            var saleItems = items.Select(item => taxes.ApplyTaxes(item)).ToList();
+            var cart = new Cart();
+            cart.Add(saleItems);
+            taxes.ApplyTaxes(cart);
+
+            // TODO > NEW FEATURE > Send notification to accounting if cart total amount exceeds 1000
+            // 1) how to test this new feature?
+            // 2) how to add new code?
+
+            SaveToDb(cart);
+        }
+    }
+}
---
+++ ApplyingTddToLegacyCode/Rule2Example/Taxes/EuropeTaxes.cs
@@ -0,0 +1,23 @@
+namespace Rule2Example.Taxes
+{
+    public class EuropeTaxes : Taxes
+    {
+        internal override SaleItem ApplyTaxes(Item item)
+        {
+            var saleItem = new SaleItem(item)
+            {
+                SalePrice = item.Price*1.2m
+            };
+            return saleItem;
+        }
+
+        internal override void ApplyTaxes(Cart cart)
+        {
+            if (cart.TotalSalePrice <= 300m) return;
+            var exclusion = 30m/cart.SaleItems.Count;
+            foreach (var item in cart.SaleItems)
+                if (item.SalePrice - exclusion > 100m)
+                    item.SalePrice -= exclusion;
+        }
+    }
+}
