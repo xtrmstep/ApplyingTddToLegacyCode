7f6e1d7321672cca1ed667050c3a9bea3624ee59
Alexander Goida <alexander.goida@gmail.com>
2016-08-17 19:45:16 +0300

    Rule 1 example

---
+++ ApplyingTddToLegacyCode/Rule1Example/Shop/EuropeShop.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Rule1Example.Taxes;
+
+namespace Rule1Example.Shop
+{
+    public class EuropeShop : Shop
+    {
+        public override void CreateSale()
+        {
+            var taxes = new EuropeTaxes();
+            var items = LoadSelectedItemsFromDb();
+            var saleItems = items.Select(item => taxes.ApplyTaxes(item)).ToList();
+            var cart = new Cart();
+            cart.Add(saleItems);
+            taxes.ApplyTaxes(cart);
+
+            // TODO > NEW FEATURE > Send notification to accounting if cart total amount exceeds 1000 -->
+            // 1) how to test?
+            // 2) hot to add?
+
+            SaveToDb(cart);
+        }
+    }
+}
