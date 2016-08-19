a3ef678047b41fa8869b3c59ed726f76f21d3c2b
Alexander Goida <alexander.goida@gmail.com>
2016-08-19 10:25:23 +0300

    Added tests for extracted classes

---
+++ ApplyingTddToLegacyCode/Rule3Example/Tests/Data/ObjectMapperTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rule3Example.Tests.Data
+{
+    public class ObjectMapperTests
+    {
+        public void Should_return_null_for_null()
+        {
+            
+        }
+
+        public void Should_return_empty_for_empty()
+        {
+            
+        }
+
+        public void Should_convert_items()
+        {
+            
+        }
+    }
+}
---
+++ ApplyingTddToLegacyCode/Rule3Example/Tests/Taxes/EuropeTaxesTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rule3Example.Tests.Taxes
+{
+    public class EuropeTaxesTests
+    {
+        public void Should_not_fail_for_null()
+        {
+            
+        }
+
+        public void Should_apply_taxes_to_items()
+        {
+            
+        }
+
+        public void Should_apply_taxes_to_whole_cart()
+        {
+            
+        }
+
+        public void Should_apply_taxes_to_whole_cart_and_change_items()
+        {
+
+        }
+    }
+}
