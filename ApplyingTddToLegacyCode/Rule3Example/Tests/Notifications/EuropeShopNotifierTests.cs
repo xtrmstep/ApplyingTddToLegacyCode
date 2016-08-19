634912826d8fd623f02666e95265ba713a357820
Alexander Goida <alexander.goida@gmail.com>
2016-08-19 10:36:23 +0300

    Added lost content

--- ApplyingTddToLegacyCode/Rule3Example/Tests/Notifications/EuropeShopNotifierTests.cs
+++ ApplyingTddToLegacyCode/Rule3Example/Tests/Notifications/EuropeShopNotifierTests.cs
@@ -2,11 +2,20 @@
 
 namespace Rule3Example.Tests.Notifications
 {
-    public class EuropeNotifierTests
+    public class EuropeShopNotifierTests
     {
         public void Should_not_send_when_less_or_equals_to_1000()
         {
+            // arrange
+            var cart = new Cart();
+            var notifier = new EuropeShopNotifier();
+            // add items to cart
 
+            // act
+            notifier.Send(cart);
+
+            // assert
+            // PROBLEM: how to verify if we don't have access to the object?
         }
 
         public void Should_send_when_greater_than_1000()
